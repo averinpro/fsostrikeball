667af8d9064560882fc37e16173a32720fc5ee0a
tbb <dmitry.tbb@yandex.ru>
2018-11-22 20:20:29 +0300

    Добавил ОС в заголовки запроса. Добавил горизонтальную ориентацию. Сделал изменение стилей при повороте. + Небольшой рефакторинг.

--- App.js
+++ App.js
@@ -1,69 +1,88 @@
 import React, { Component } from 'react';
-import { StyleSheet, WebView, Platform, ActivityIndicator, View, Text, Button} from 'react-native';
+import { 
+  View, 
+  Text,
+  StyleSheet, 
+  WebView, 
+  Platform, 
+  ActivityIndicator,
+  Dimensions
+} from 'react-native';
+import { ScreenOrientation } from 'expo'
 
 export default class MainActivity extends Component {
+  constructor() {
+    super();
+    const isPortrait = () => {
+      const dim = Dimensions.get('screen');
+      return dim.height >= dim.width;
+    };
+
+    this.state = {
+      orientation: isPortrait() ? 'portrait' : 'landscape'
+    };
+    
+    // Event Listener for orientation changes
+    Dimensions.addEventListener('change', () => {
+      this.setState({
+        orientation: isPortrait() ? 'portrait' : 'landscape'
+      });
+    });
+  }
+  componentWillMount() {
+    ScreenOrientation.allowAsync(Expo.ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN);
+  }
   ActivityIndicatorLoadingView() {
     return (
       <ActivityIndicator
         color='#FFFFFF'
         size='large'
-        style={styles.ActivityIndicatorStyle}
+        style={ [styles.container, styles.vertical] }
       />
     );
   }
+  ErrorHandlerView() {
+    return (
+      <View style={ [styles.container, styles.vertical] }>
+        <Text>Что-то пошло не так...</Text>
+      </View>
+    );
+  }
   render() {
-    let WebViewRef;
     return (
       <WebView 
-      style={styles.WebViewStyle} 
-      //(Platform.OS) Добавить в URL
-      ref={WEBVIEW_REF => (WebViewRef = WEBVIEW_REF)}
-      source={{uri: 'https://fsostrikeball.ru?app=1'}} 
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      decelerationRate="normal"
-      automaticallyAdjustContentInsets={false}
-      onNavigationStateChange={this._onNavigationStateChange}
-      renderLoading={this.ActivityIndicatorLoadingView} 
-      onShouldStartLoadWithRequest={this._onShouldStartLoadWithRequest}
-      startInLoadingState={true}  
-      scalesPageToFit={true}
-      renderError={()=>{
-        return (
-          <View>
-          <Text>
-            У нас возникли проблемы с подключением к интернету. Давайте проверим его работе и повторим попытку еще раз
-          </Text>
-          <Button
-            onpress={()=>{ WebViewRef && WebViewRef.reload();}}
-            title="Обновить"
-            color="#841584"
-            accessibilityLabel="Попробовать еще раз"
-          />
-          </View>
-        )
-      }}
+      style={[
+        styles.container, 
+        styles.vertical, 
+        this.state.orientation === 'portrait' ? styles.portraitStyle : styles.landscapeStyle
+      ]}
+      source={{
+        uri: 'https://fsostrikeball.ru?app=1',
+        headers: { "OS": Platform.OS },
+      }} 
+      automaticallyAdjustContentInsets={ false }
+      startInLoadingState = { true }
+      renderLoading={ this.ActivityIndicatorLoadingView } 
+      renderError={ this.ErrorHandlerView }
       />
     );
   }
 }
    
 const styles = StyleSheet.create({
-  WebViewStyle: {
+  container: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    flex:1,
-    marginTop: Platform.OS === 'ios' ? 20 : 10
   },
-
-  ActivityIndicatorStyle: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      alignItems: 'center',
-      backgroundColor: 'black',
-      justifyContent: 'center'
+  vertical: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  portraitStyle: {
+    marginTop: (Platform.OS) === 'ios' ? 20 : 0,
+  },
+  landscapeStyle: {
+    marginTop: 0,
   }
 });
